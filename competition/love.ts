f63162d297d8ba7400d50ddeefd741c0085d4a42
22-pig <845501558@qq.com>
2021-10-06 15:02:25 +0800

    比赛

---
+++ competition/love.ts
@@ -0,0 +1,9 @@
+// 有一种感动,叫"中国红"
+let Sensation: string = "Chinese red";
+// 有一种骄傲,叫"五星红旗"
+let Pride: string = "Five-starred Red Flag";
+// 有一种表白,叫"我爱你中国"
+let Confession: string = "I Love you, China";
+
+
+// 飞鸟和天空，游鱼和海洋，我和我的祖国
